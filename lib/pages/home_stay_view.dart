bfdb027f3072442527dac0635e150840ff72a717
iamsajidhsaz <iamsajid.aa@gmail.com>
2024-05-25 23:48:48 +0530

    25052024

--- lib/pages/home_stay_view.dart
+++ lib/pages/home_stay_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:slider_button/slider_button.dart';
 
 class HomeStayView extends StatelessWidget {
   final List<String> facils;
